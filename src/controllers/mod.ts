f7aaeff2b4e9586c32a841f4bc1ce5314e31ddbc
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-09-10 09:01:34 -0300

    Files updated.

--- src/controllers/mod.ts
+++ src/controllers/mod.ts
@@ -1,7 +1,3 @@
 export * from './account/mod.ts';
 export * from './authentication/mod.ts';
-export * from './characteristics/mod.ts';
-export * from './contact/mod.ts';
-export * from './match/mod.ts';
-export * from './plan/mod.ts';
 export * from './user/mod.ts';

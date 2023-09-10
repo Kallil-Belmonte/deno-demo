7b110aacfea6285ec6c2e6b62660a8c3600d0ebe
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-09-10 13:02:07 -0300

    Files updated.

--- src/controllers/account/createAccount.ts
+++ src/controllers/account/createAccount.ts
@@ -1,5 +1,5 @@
 import { success } from '@/routes/_files/responses.ts';
 
-const createAccount = (request: Request) => success(`Endpoint: account`);
+const createAccount = (request: Request) => success(`Endpoint: createAccount`);
 
 export default createAccount;
--- src/controllers/account/deleteAccount.ts
+++ src/controllers/account/deleteAccount.ts
@@ -1,5 +1,5 @@
 import { success } from '@/routes/_files/responses.ts';
 
-const deleteAccount = (request: Request) => success(`Endpoint: account`);
+const deleteAccount = (request: Request) => success(`Endpoint: deleteAccount`);
 
 export default deleteAccount;
